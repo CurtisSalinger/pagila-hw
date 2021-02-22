477fa735b928f624834606f551d7549c829c2001
CurtisSalinger <curtisjacksalinger@gmail.com>
2021-02-22 15:28:22 -0800

    all working

--- sql/10.sql
+++ sql/10.sql
@@ -4,4 +4,4 @@
  *
  */
 
- SELECT film.title, film_actor.film_id, COUNT(film_actor.actor_id) as actor_count FROM film JOIN film_actor ON film.film_id = film_actor.film_id GROUP BY film.title, film_actor.film_id HAVING count(film_actor.actor_id) > 0 ORDER BY COUNT(film_actor.actor_id), film_actor.film_id, film.title;
+ SELECT film.title, film_actor.film_id, COUNT(film_actor.actor_id) as actor_count FROM film JOIN film_actor ON film.film_id = film_actor.film_id GROUP BY film.title, film_actor.film_id HAVING count(film_actor.actor_id) > 0 ORDER BY COUNT(film_actor.actor_id), film.title;
